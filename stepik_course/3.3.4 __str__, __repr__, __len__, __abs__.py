07609b8e3097a34e31a89552f322bb64c59548fc
Victor Smirnov <125829316+Victor-Smirnoff@users.noreply.github.com>
2023-04-02 15:35:24 +0300

    Create 3.3.4 __str__, __repr__, __len__, __abs__.py

---
+++ stepik_course/3.3.4 __str__, __repr__, __len__, __abs__.py
@@ -0,0 +1,27 @@
+class WordString:
+    def __init__(self, value=''):
+        self.__string = value
+        self.lst = []
+
+    @property
+    def string(self):
+        return self.__string
+
+    @string.setter
+    def string(self, value):
+        self.__string = value
+
+    def __len__(self):
+        return len(self.string.split())
+
+    def __call__(self, indx, *args, **kwargs):
+        return self.string.split()[indx]
+
+
+
+words = WordString()
+words.string = "Курс по Python ООП"
+n = len(words)
+first = "" if n == 0 else words(0)
+print(words.string)
+print(f"Число слов: {n}; первое слово: {first}")
